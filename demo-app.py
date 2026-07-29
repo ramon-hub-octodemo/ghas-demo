4448cf8f679f6c82d2f5f528caf909bd315642e1
Ramon <47693756+ramon-hub@users.noreply.github.com>
2026-07-29 07:37:48 +0000

    Add config

--- demo-app.py
+++ demo-app.py
@@ -23,7 +23,7 @@ class Config:
     DATABASE_URL = "postgres://demo_user:placeholder@localhost:5432/demo_db"
 
     # <<< SWAP THIS LINE >>>
-    AWS_ACCESS_KEY_ID = "11ALL37PA0NICv1Nnd4wxo_dyNslty3VM7SNleAXP7XwvByc7oSZh4iw7FHttaPkZC5L5V6ZLHM112wss"
+    AWS_ACCESS_KEY_ID = "11ALL37PA0NICv1Nnd4wxo_dyNslty3VM7SNleAXP7XwvByc99811QSZh4iw7FHttaPkZC5L5V6ZLHM112wss"
 
     LOG_LEVEL = "debug"
     FEATURE_FLAG_NEW_UI = True

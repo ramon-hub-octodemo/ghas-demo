01c8c52ea8f20e9eb9eb005710e5893b118b3771
Ramon <47693756+ramon-hub@users.noreply.github.com>
2026-07-29 08:35:58 +0000

    adding secret

--- demo-app.py
+++ demo-app.py
@@ -23,7 +23,7 @@ class Config:
     DATABASE_URL = "postgres://demo_user:placeholder@localhost:5432/demo_db"
 
     # <<< SWAP THIS LINE >>>
-    should_not_be_here_token= "add to keyvault"
+    AWS_ACCESS_KEY_ID= "add to keyvault"
 
     LOG_LEVEL = "debug"
     FEATURE_FLAG_NEW_UI = True
